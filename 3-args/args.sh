2f6b76557bc3b41acb9149786d875dbc533d6982
SajjdManafi <sajadmanafi8@gmail.com>
2021-12-24 15:13:01 +0330

    use command line Arguments

---
+++ 3-args/args.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "\$0 is $0"
+echo "\$1 is $1"
+echo "\$2 is $2"
+echo "\$3 is $3"
+echo "\$* is $*"
+echo "\$# is $#"
+
+
+#./args.sh ubunutu mac arch 
+#  $0 is ./args.sh
+#  $1 is ubunutu
+#  $2 is mac
+#  $3 is arch
+#  $* is ubunutu mac arch
+#  $# is 3
