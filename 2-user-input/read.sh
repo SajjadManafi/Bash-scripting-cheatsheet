9a03d4b852e3d7fdc77e08be13e2e2b3ae305f8c
SajjdManafi <sajadmanafi8@gmail.com>
2021-12-24 15:09:34 +0330

    user input in bash

---
+++ 2-user-input/read.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# wait for user input (var number)
+read number
+echo $number
+
+
+# print text and wait for user input (var name)
+read -p "Enter a Your name: " name
+echo "Your name is : $name"
